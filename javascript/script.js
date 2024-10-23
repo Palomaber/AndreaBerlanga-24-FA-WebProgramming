76cc66c9957fda973a26afb14e9b7692b7d964b9
Andrea <palomaberlanga08@gmail.com>
2024-10-22 20:30:12 -0500

    added javascript
    updated

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+//Variables
+
+//First and Last Name Variables
+var fname = "Andrea";
+var lname = "Berlanga";
+
+//Sent Variables
+var sentence ="Find your recipe with your Warm Wonders";
+//thetruth variable declaration
+var theTruth = true;
+
+//variables for 10, 15, 25
+var numTen = 10;
+var numFifteen = 15;
+var numTwentytFive = 25;
+
+//concatenation to display the first and last name
+console.log(fname + " " + lname);
+
+//display one of the sentence variable's length
+console.log("The length of my sentence is " + sentence.length + " characters long.");
+//we want to display our message.
+console.log("The sum of " + numTen + " and " + numFifteen + " is equal to " + numTwentytFive + ".");
+//display the truth storing//
+console.log(Truth = ("The variable theTruth is holding the value of " +Boolean (theTruth) + "The data type of theTruth is " +typeof theTruth + "."));
+
+
