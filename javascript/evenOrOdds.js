91352a3c050497d6908a80d68b5b0bde2f34eb2f
Andrea <palomaberlanga08@gmail.com>
2024-11-07 10:57:08 -0600

    Update evenOrOdds.js
    update evenOrOdds.js file alerts

--- javascript/evenOrOdds.js
+++ javascript/evenOrOdds.js
@@ -1,41 +1,35 @@
-//
-var one = prompt ("Please inpute a number between 1 and 10");
-var two = prompt ("Please inpute a number between 1 and 10"); 
-//
-parse1 = parseInt(one);
+// projected sentence
+var one = prompt("Please input a number between 1 and 10");
+var two = prompt("Please input a number between 1 and 10"); 
+// turn values into numbers
+var parse1 = parseInt(one);
 console.log(parse1);
-parse2 = parseInt(two);
+var parse2 = parseInt(two);
 console.log(parse2);
-//
-var redostat = "Your value(s) did not meet the requirments";
-//
-while (isNaN(parse1) || parse1 < 1 || parse1 > 10){
-//
-alert(redostat);
+// message for invalid input
+var fix = "Your value(s) did not meet the requirements";
+// confirm first second number
+while (isNaN(parse1) || parse1 < 1 || parse1 > 10) {
+alert(fix);
 one = prompt("Please input a number between 1 and 10 again");
 parse1 = parseInt(one);
 console.log(parse1);
 }
-//
-while (isNaN(parse2) || parse2 < 1 || parse2 > 10){
-//
-alert(redostat);
-one = prompt("Please input a number between 20 and 50 again");
+// confirm second second number
+while (isNaN(parse2) || parse2 < 1 || parse2 > 10) {
+alert(fix);
+two = prompt("Please input a number between 1 and 10 again");
 parse2 = parseInt(two);
 console.log(parse2);
 }
-//
+// multipy inputed numbers
 let product = parse1 * parse2;
-//
-var odd = "This number is odd ";
-var even = "This number is even";
-//
-if (product % 2 == 0){
-alert("product + is even");
-console.log ("product + is even");
+// figuring out if multiplied numbers are even or odd
+if (product % 2 === 0) {
+alert(product + " is even");
+console.log(product + " is even");
+} else {
+alert(product + " is odd");
+console.log(product + " is odd");
 }
-else {
-    alert(product + "is odd");
-    console.log (product + "is odd");
-}
-;
+
